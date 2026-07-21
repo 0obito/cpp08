fae45eea6531b16efb7402b3bb5e22062409e8ba
0obito <elmsafer.abdellah00@gmail.com>
2026-07-21 16:37:14 +0100

    Should be done

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -2,18 +2,16 @@
 #define EASYFIND_HPP
 
 #include <algorithm>
-#include <iterator>
-#include <exception>
 #include <stdexcept>
 #include <iostream>
 
 template <typename T>
-typename T::iterator easyfind(T& t, int i) {
+void easyfind(T& t, int i) {
     typename T::iterator it;
     it = std::find(t.begin(), t.end(), i);
     if (it == t.end())
         throw std::out_of_range("element was not found in the container.");
-    return it;
+    std::cout<<"value: "<<*it<<" was found at index: "<<std::distance(t.begin(), it)<<"\n";
 }
 
 #endif /*EASYFIND_HPP*/
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,10 +1,12 @@
 #include "easyfind.hpp"
 #include <vector>
+#include <iostream>
+#include <list>
 
 int main() {
     std::vector<int> vec;
     vec.reserve(5);
-    vec.push_back(6);
+    vec.push_back(0);
     vec.push_back(8);
     vec.push_back(2);
     vec.push_back(4);
@@ -12,9 +14,7 @@ int main() {
 
     std::cout<<"========== Test 1: int vector (element doesn't exist) ==========\n\n";
     try {
-        std::vector<int>::iterator it;
-        it = easyfind(vec, 1);
-        std::cout<<"the value: "<<*it<<" was found at index: "<<vec.begin() - it<<"\n";
+        easyfind(vec, 1);
     }
     catch (std::out_of_range& e) {
         std::cout<<"Caught Exception: "<<e.what()<<"\n";
@@ -22,23 +22,34 @@ int main() {
 
     std::cout<<"\n========== Test 2: int vector (element exists) ==========\n\n";
     try {
-        std::vector<int>::iterator it;
-        it = easyfind(vec, 6);
-        std::cout<<"the value: "<<*it<<" was found at index: "<<it - vec.begin()<<"\n";
+        easyfind(vec, 4);
     }
     catch (std::out_of_range& e) {
         std::cout<<"Caught Exception: "<<e.what()<<"\n";
     }
 
-    std::cout<<"\n========== Test 3: int  (element exists) ==========\n\n";
+    std::list<int> l;
+    l.push_back(2);
+    l.push_back(1);
+    l.push_back(4);
+    l.push_back(3);
+    l.push_back(6);
+    l.push_back(5);
+
+    std::cout<<"\n========== Test 3: int list (element exists) ==========\n\n";
     try {
-        std::vector<int>::iterator it;
-        it = easyfind(vec, 6);
-        std::cout<<"the value: "<<*it<<" was found at index: "<<it - vec.begin()<<"\n";
+        easyfind(l, 5);
     }
     catch (std::out_of_range& e) {
         std::cout<<"Caught Exception: "<<e.what()<<"\n";
     }
 
+    std::cout<<"\n========== Test 4: int list (element doesn't exist) ==========\n\n";
+    try {
+        easyfind(l, 9);
+    }
+    catch (std::out_of_range& e) {
+        std::cout<<"Caught Exception: "<<e.what()<<"\n";
+    }
     std::cout<<"\n========== End of tests ==========\n";
 }
