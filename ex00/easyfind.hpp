b8bd5997c35d5569cfbd435b95399956cb279f66
0obito <elmsafer.abdellah00@gmail.com>
2026-07-21 15:40:17 +0100

    EX00 is essentially done.

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -2,20 +2,18 @@
 #define EASYFIND_HPP
 
 #include <algorithm>
-// #include <iterator>
+#include <iterator>
 #include <exception>
 #include <stdexcept>
 #include <iostream>
 
 template <typename T>
-void easyfind(T t, int i) {
-    T::iterator it;
-    it = std::find(T.begin(), T.end(), i);
-    if (it == T.end())
-        return 
-    if (it == T.end())
-        throw std::out_of_range();
-    std::cout<<*it<<"\n";
+typename T::iterator easyfind(T& t, int i) {
+    typename T::iterator it;
+    it = std::find(t.begin(), t.end(), i);
+    if (it == t.end())
+        throw std::out_of_range("element was not found in the container.");
+    return it;
 }
 
 #endif /*EASYFIND_HPP*/
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,44 @@
 #include "easyfind.hpp"
+#include <vector>
 
 int main() {
-    ;
+    std::vector<int> vec;
+    vec.reserve(5);
+    vec.push_back(6);
+    vec.push_back(8);
+    vec.push_back(2);
+    vec.push_back(4);
+    vec.push_back(6);
+
+    std::cout<<"========== Test 1: int vector (element doesn't exist) ==========\n\n";
+    try {
+        std::vector<int>::iterator it;
+        it = easyfind(vec, 1);
+        std::cout<<"the value: "<<*it<<" was found at index: "<<vec.begin() - it<<"\n";
+    }
+    catch (std::out_of_range& e) {
+        std::cout<<"Caught Exception: "<<e.what()<<"\n";
+    }
+
+    std::cout<<"\n========== Test 2: int vector (element exists) ==========\n\n";
+    try {
+        std::vector<int>::iterator it;
+        it = easyfind(vec, 6);
+        std::cout<<"the value: "<<*it<<" was found at index: "<<it - vec.begin()<<"\n";
+    }
+    catch (std::out_of_range& e) {
+        std::cout<<"Caught Exception: "<<e.what()<<"\n";
+    }
+
+    std::cout<<"\n========== Test 3: int  (element exists) ==========\n\n";
+    try {
+        std::vector<int>::iterator it;
+        it = easyfind(vec, 6);
+        std::cout<<"the value: "<<*it<<" was found at index: "<<it - vec.begin()<<"\n";
+    }
+    catch (std::out_of_range& e) {
+        std::cout<<"Caught Exception: "<<e.what()<<"\n";
+    }
+
+    std::cout<<"\n========== End of tests ==========\n";
 }
