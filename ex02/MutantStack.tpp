e7efa3a567ee1cde5f5627f0c6066e31fff8eed1
0obito <elmsafer.abdellah00@gmail.com>
2026-07-30 11:42:26 +0100

    EX02 is DONE. And so is everything!

--- ex02/MutantStack.tpp
+++ ex02/MutantStack.tpp
@@ -12,11 +12,6 @@ MutantStack<T>& MutantStack<T>::operator=(const MutantStack& other) {
     return *this;
 }
 
-// typedef typename std::stack<T>::container_type::iterator iterator;
-// typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-// typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-// typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
-
 template <typename T>
 MutantStack<T>::~MutantStack() {}
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,9 +1,18 @@
-#include "MutantStack.hpp"
 #include <iostream>
-#include <string>
+#include <list>
+#include "MutantStack.hpp"
 
-int main()
-{
+void printTitle(const std::string& title) {
+    std::cout << "\n========================================\n";
+    std::cout << "  " << title << "  \n";
+    std::cout << "========================================\n";
+}
+
+
+int main() {
+
+    printTitle("test 1: Subject page tests");
+
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
@@ -19,10 +28,94 @@ int main()
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
-    while (it != ite) {
+    while (it != ite)
+    {
         std::cout << *it << std::endl;
         ++it;
     }
     std::stack<int> s(mstack);
+
+    // ===========================================================================================================
+
+    printTitle("test 2: Subject page tests but this time using std::list");
+
+    std::list<int> mlist;
+    mlist.push_back(5);
+    mlist.push_back(17);
+    std::cout << mlist.back() << std::endl;
+    mlist.pop_back();
+    std::cout << mlist.size() << std::endl;
+    mlist.push_back(3);
+    mlist.push_back(5);
+    mlist.push_back(737);
+    //[...]
+    mlist.push_back(0);
+    std::list<int>::iterator itt = mlist.begin();
+    std::list<int>::iterator itte = mlist.end();
+    ++itt;
+    --itt;
+    while (itt != itte)
+    {
+        std::cout << *itt << std::endl;
+        ++itt;
+    }
+    std::list<int> l(mlist);
+
+    // ===========================================================================================================
+
+    printTitle("test 3: Reverse Iterators");
+
+    MutantStack<int> ms;
+    for (int i = 1; i <= 5; ++i)
+        ms.push(i * 10);
+
+    std::cout << "Normal iteration:  ";
+    for (MutantStack<int>::iterator it = ms.begin(); it != ms.end(); ++it)
+        std::cout << *it << " ";
+    std::cout << "\n";
+
+    std::cout << "Reverse iteration:  ";
+    for (MutantStack<int>::reverse_iterator rit = ms.rbegin(); rit != ms.rend(); ++rit)
+        std::cout << *rit << " ";
+    std::cout << std::endl;
+
+    // ===========================================================================================================
+
+    printTitle("test 4: Orthodox Canonical Form");
+
+    MutantStack<int> original;
+    original.push(100);
+    original.push(200);
+
+    MutantStack<int> copy(original);
+    MutantStack<int> assigned;
+    assigned = original;
+
+    copy.push(300);
+    assigned.pop();
+
+    std::cout << "Original size: " << original.size() << "\n";
+    std::cout << "Copy size:     " << copy.size() << "\n";
+    std::cout << "Assigned size: " << assigned.size() << "\n";
+
+    // ===========================================================================================================
+
+    printTitle("test 5: String stack testing");
+
+    MutantStack<std::string> strStack;
+    strStack.push("AAA");
+    strStack.push("BBBBB");
+    strStack.push("CCCCC");
+    strStack.push("DDDDDDDDDD");
+
+    for (MutantStack<std::string>::iterator it = strStack.begin(); it != strStack.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << "\n";
+
+    std::cout << "\n========================================\n";
+    std::cout << "        all tests are done\n";
+    std::cout << "========================================\n";
+
     return 0;
 }
